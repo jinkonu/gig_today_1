bee6d02c358143caa44aa024c38bd5229313621e
Jin Geonwoo <jinkonu99@gmail.com>
2024-02-23 21:26:40 +0900

    feat: 메인 App 파일 추가

---
+++ lib/presenter/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
+
+import 'navigation/navigation.dart';
+
+class GigTodayApp extends StatelessWidget {
+  final AppRouter _router = AppRouter();
+
+  GigTodayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterWebFrame(
+      maximumSize: const Size(400, 800),
+      backgroundColor: Colors.black12,
+      builder: (_) => MaterialApp.router(
+        title: "Gig Today",
+        routerConfig: _router.config(),
+        scrollBehavior: AppScrollBehavior(),
+      )
+    );
+  }
+}
+
+class AppScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse,
+    PointerDeviceKind.trackpad,
+  };
+}
