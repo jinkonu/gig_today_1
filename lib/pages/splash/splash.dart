2624f6e46c6a9e0e7a57fc802e52967f0684cff7
Jin Geonwoo <jinkonu99@gmail.com>
2024-02-23 22:38:32 +0900

    feat: Splash 페이지에 애니메이션 추가

--- lib/pages/splash/splash.dart
+++ lib/pages/splash/splash.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:gig_today_1/presenter/widgets/loading.dart';
 
 @RoutePage()
 class SplashPage extends StatefulWidget {
@@ -13,14 +14,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-          ],
-        ),
-      ),
+      body: LoadingIndicator(),
     );
   }
 }
---
+++ lib/presenter/widgets/loading.dart
@@ -0,0 +1,57 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+
+import '../gen/assets.gen.dart';
+
+class LoadingIndicator extends StatefulWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  State<LoadingIndicator> createState() => _LoadingIndicatorState();
+}
+
+class _LoadingIndicatorState extends State<LoadingIndicator> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    _animation = Tween<double>(
+      begin: 0,
+      end: 360,
+    ).animate(_controller);
+
+    _controller.repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: _animation.value * math.pi / 50,
+            child: Image(
+              image: Assets.images.logo.provider(),
+              width: 200,
+            ),
+          );
+        }
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
