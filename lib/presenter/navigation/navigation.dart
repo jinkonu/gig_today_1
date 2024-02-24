5e2173865683bc81b3f816232252d7cb3d2f853b
Jin Geonwoo <jinkonu99@gmail.com>
2024-02-24 21:25:40 +0900

    feat: 장소 페이지 추가

--- lib/presenter/navigation/navigation.dart
+++ lib/presenter/navigation/navigation.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 
+import '../../pages/gig/sections/gig.dart';
 import '../../pages/home/home.dart';
 import '../../pages/splash/splash.dart';
+import '../../pages/venue/venue.dart';
 
 part 'navigation.gr.dart';
 
@@ -11,7 +13,9 @@ class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
     AutoRoute(path: "/", page: SplashRoute.page),
-    AutoRoute(path: "/home", page: HomeRoute.page)
+    AutoRoute(path: "/home", page: HomeRoute.page),
+    AutoRoute(path: "/venue/:id", page:VenueRoute.page),
+    AutoRoute(path: "/gig/:id", page: GigRoute.page),
   ];
 
   @override
